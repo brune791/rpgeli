e5832969e38a4766a1af378e58a39a666f4dfc8b
Eli <brune791@hsd.k12.or.us>
2023-06-12 09:57:21 -0700

    adding more

--- mainframework/storyline.py
+++ mainframework/storyline.py
@@ -1,7 +1,7 @@
 from story_func import character_Creation
 from story_func import story_Two_Questions_Base
 from story_func import storyline_preset_V2
-
+from story_func import Room
 
 def chapter_One():
     #character_Creation()
@@ -21,9 +21,15 @@ def chapter_One():
         
         print("this is the end of demo mr teacher i was not able to get the level up system to work and i could not ask you sense i have been sick all this week and missed school im sorry")
 
-    """
+   """ """
     storyline_preset_V2(("you wake up and find your self in a dimly lit room","as you stand up and look around you see a big metal door made of hard iron",
-    "1: go to door?","2: stay were you are.", "you make your way to the door and try to open it","you stay and stand around boring "))
+    "1: go to door?","2: stay were you are.", "you make your way to the door and try to open it","you stay and stand around boring "))"""
     "chapter_One()"
+    wakeup_room_actions = ["search room "," skills ","chill "]
+    wakeup_room_storyline=["you wake up and find your self in a dimly lit room","as you stand up and look around you see a big metal door made of hard iron"]
+    wakeup_room = Room("wakeup_room", wakeup_room_actions,wakeup_room_storyline)
+    wakeup_room.display_user_Input
+    wakeup_room.connectedRooms = []
+    pass
 
 chapter_One()
